61e694c9dceed28de11fad87fd40a6ee9a34e1de
TeacherSpaceSchool <azyk.store@gmail.com>
2022-02-17 02:52:56 +0600

    check

--- src/lib.js
+++ src/lib.js
@@ -6,11 +6,11 @@ export const checkMobile = (ua)=>{
 
 export const openScanner = ({idx, path})=>{
     let now = new Date()
-    window.location = `salykscanner://path?idx=${idx}&path=${path}`
     setTimeout(()=>{
-        if (new Date() - now > 100) return;
+        if (new Date() - now > 600) return;
         window.location = 'https://play.google.com/store/apps/details?id=com.salyk.scanner'
-    },25)
+    },500)
+    window.location = `salykscanner://path?idx=${idx}&path=${path}`
 }
 
 export const getJWT = (cookie)=>{
