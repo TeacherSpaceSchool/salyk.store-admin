baa0a5fbb3cefb778b7644161cbd06c9ae8b1e31
TeacherSpaceSchool <azyk.store@gmail.com>
2022-04-26 12:10:58 +0600

    check

--- components/dialog/Confirmation.js
+++ components/dialog/Confirmation.js
@@ -6,38 +6,42 @@ import { bindActionCreators } from 'redux'
 import * as appActions from '../../redux/actions/app'
 import * as mini_dialogActions from '../../redux/actions/mini_dialog'
 import * as snackbarActions from '../../redux/actions/snackbar'
-import Done from '@material-ui/icons/Done';
-import Clear from '@material-ui/icons/Clear';
-import IconButton from '@material-ui/core/IconButton';
 import dialogContentStyle from '../../src/styleMUI/dialogContent'
+import Button from '@material-ui/core/Button';
 
 const Confirmation =  React.memo(
     (props) =>{
         const { showMiniDialog } = props.mini_dialogActions;
         const { showSnackBar } = props.snackbarActions;
         const { showLoad } = props.appActions;
-        const { classes, action } = props;
+        const { classes, action, actionCancel } = props;
         return (
-            <div className={classes.line}>
-                <IconButton onClick={async()=>{
-                    await showMiniDialog(false)
-                    await showLoad(true)
-                    try {
-                        await action()
-                    }  catch (err) {
-                        console.error(err)
-                        showSnackBar('Ошибка', 'error')
-                    }
-                    await showLoad(false)
-                }} aria-label='Delete'>
-                    <Done className={classes.button} />
-                </IconButton>
-                <IconButton onClick={async()=>{
-                    showMiniDialog(false)
-                }} aria-label='Cancel'>
-                    <Clear className={classes.button}/>
-                </IconButton>
-            </div>
+            action?
+                <center className={classes.line}>
+                    <Button variant='outlined' size='large' color='primary' style={{marginRight: 20}} onClick={async()=>{
+                        await showMiniDialog(false)
+                        await showLoad(true)
+                        try {
+                            await action()
+                        }  catch (err) {
+                            console.error(err)
+                            showSnackBar('Ошибка', 'error')
+                        }
+                        await showLoad(false)
+                    }}>
+                        ДА
+                    </Button>
+                    <Button variant='outlined' color='secondary' size='large' onClick={async()=>{
+                        showMiniDialog(false)
+                        if(actionCancel){
+                            await actionCancel()
+                        }
+                    }}>
+                        НЕТ
+                    </Button>
+                </center>
+                :
+                null
         );
     }
 )
